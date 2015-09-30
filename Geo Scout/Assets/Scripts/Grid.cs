2c1b05223b433df8e4e844f621b294211f6ae17a
ewatkins2010 <eddiewattz@gmail.com>
2015-09-30 16:51:35 -0400

    attempting resolution fix

--- Geo Scout/Assets/Scripts/Grid.cs
+++ Geo Scout/Assets/Scripts/Grid.cs
@@ -20,20 +20,20 @@ public class Grid : MonoBehaviour {
 	void SpawnDirt(){
 		dirtIndex = 0;
 		for (int i = 0; i < 23; i++) {
-			for (int j = 0; j < 130; j++){
+			for (int j = 0; j < 122; j++){
 				Instantiate(dirt[dirtIndex], new Vector3(startPos1.x + (j*2), startPos1.y - (i*2), 0), transform.rotation);
 			}
 		}
 
 		dirtIndex = 1;
 		for (int i = 23; i < 46; i++) {
-			for (int j = 0; j < 130; j++){
+			for (int j = 0; j < 122; j++){
 				Instantiate(dirt[dirtIndex], new Vector3(startPos1.x + (j*2), startPos1.y - (i*2), 0), transform.rotation);
 			}
 		}
 		dirtIndex = 2;
 		for (int i = 46; i < 70; i++) {
-			for (int j = 0; j < 130; j++){
+			for (int j = 0; j < 122; j++){
 				Instantiate(dirt[dirtIndex], new Vector3(startPos1.x + (j*2), startPos1.y - (i*2), 0), transform.rotation);
 			}
 		}
