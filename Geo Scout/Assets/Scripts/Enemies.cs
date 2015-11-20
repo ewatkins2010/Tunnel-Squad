4fd61dbacf12db1ff6cd3232e92c892733b5f4c8
KatieBolster <katie.bolster@yahoo.com>
2015-11-20 10:53:15 -0500

    Enemy Sprite Changes Direction on Impact

--- Geo Scout/Assets/Scripts/Enemies.cs
+++ Geo Scout/Assets/Scripts/Enemies.cs
@@ -18,12 +18,17 @@ public class Enemies : MonoBehaviour {
     float myWidth;
 	GameObject player;
 
+    
+
 
     // Use this for initialization
     void Start () {
 		canUp = canDown = canLeft = canRight = false;
 		isMoving = false;
 		player = GameObject.Find ("Player");
+
+        
+
     }
 	
 	// Update is called once per frame
@@ -31,81 +36,112 @@ public class Enemies : MonoBehaviour {
 		Move ();
     }
 
-	public void CheckDirection(){
-		Vector2 upStart, leftStart, downStart, rightStart;
+    public void CheckDirection()
+    {
+        Vector2 upStart, leftStart, downStart, rightStart;
 
-		upStart = new Vector2 (transform.position.x, transform.position.y + 5f);
-		leftStart = new Vector2 (transform.position.x - 5f, transform.position.y);
-		downStart = new Vector2 (transform.position.x, transform.position.y - 5f);
-		rightStart = new Vector2 (transform.position.x + 5f, transform.position.y);
+        upStart = new Vector2(transform.position.x, transform.position.y + 5f);
+        leftStart = new Vector2(transform.position.x - 5f, transform.position.y);
+        downStart = new Vector2(transform.position.x, transform.position.y - 5f);
+        rightStart = new Vector2(transform.position.x + 5f, transform.position.y);
 
-		Debug.DrawRay(upStart, Vector2.up*10f, Color.red);
-		Debug.DrawRay(downStart, Vector2.down*10f, Color.red);
-		Debug.DrawRay(leftStart, Vector2.left*10f, Color.red);
-		Debug.DrawRay(rightStart, Vector2.right*10f, Color.red);
+        Debug.DrawRay(upStart, Vector2.up * 10f, Color.red);
+        Debug.DrawRay(downStart, Vector2.down * 10f, Color.red);
+        Debug.DrawRay(leftStart, Vector2.left * 10f, Color.red);
+        Debug.DrawRay(rightStart, Vector2.right * 10f, Color.red);
 
-		RaycastHit2D upHit = Physics2D.Raycast (upStart, Vector2.up, 9f);
-		if (upHit.collider != null) {
-			if (upHit.collider.gameObject.tag == "Dirt" || upHit.collider.gameObject.tag == "Barrier"){
-				canUp = false;
-			}
-			else
-				canUp = true;
-		}
-		else
-			canUp = true;
+        RaycastHit2D upHit = Physics2D.Raycast(upStart, Vector2.up, 9f);
+        if (upHit.collider != null)
+        {
+            if (upHit.collider.gameObject.tag == "Dirt" || upHit.collider.gameObject.tag == "Barrier")
+            {
+                canUp = false;
+            }
+            else
+                canUp = true;
+        }
+        else
+            canUp = true;
 
-		RaycastHit2D downHit = Physics2D.Raycast (downStart, Vector2.down, 9f);
-		if (downHit.collider != null) {
-			if (downHit.collider.gameObject.tag == "Dirt" || downHit.collider.gameObject.tag == "Barrier") {
-				canDown = false;
-			} else 
-				canDown = true;
-		} 
-		else
-			canDown = true;
+        RaycastHit2D downHit = Physics2D.Raycast(downStart, Vector2.down, 9f);
+        if (downHit.collider != null)
+        {
+            if (downHit.collider.gameObject.tag == "Dirt" || downHit.collider.gameObject.tag == "Barrier")
+            {
+                canDown = false;
+            }
+            else
+                canDown = true;
+        }
+        else
+            canDown = true;
 
-		RaycastHit2D leftHit = Physics2D.Raycast (leftStart, Vector2.left, 9f);
-		if (leftHit.collider != null) {
-			if (leftHit.collider.gameObject.tag == "Dirt" || leftHit.collider.gameObject.tag == "Barrier") {
-				canLeft = false;
-			}
-			else
-				canLeft = true;
-		} 
-		else
-			canLeft = true;
+        RaycastHit2D leftHit = Physics2D.Raycast(leftStart, Vector2.left, 9f);
+        if (leftHit.collider != null)
+        {
+            if (leftHit.collider.gameObject.tag == "Dirt" || leftHit.collider.gameObject.tag == "Barrier")
+            {
+                canLeft = false;
+                Vector3 theScale = transform.localScale;
 
-		RaycastHit2D rightHit = Physics2D.Raycast (rightStart, Vector2.right, 9f);
-		if (rightHit.collider != null) {
-			if (rightHit.collider.gameObject.tag == "Dirt" || rightHit.collider.gameObject.tag == "Barrier"){
-				canRight = false;
-			}
-			else 
-				canRight = true;
-		}
-		else 
-			canRight = true;
 
-		Vector2 upTarget = new Vector2 (transform.position.x, transform.position.y + 10f);
-		Vector2 downTarget = new Vector2 (transform.position.x, transform.position.y - 10f);
-		Vector2 leftTarget = new Vector2 (transform.position.x - 10f, transform.position.y);
-		Vector2 rightTarget = new Vector2 (transform.position.x + 10f, transform.position.y);
+                //turns scale negative on wall impact (to change sprite direction)
+                if (!canLeft && theScale.x > 0)
+                {
+                    theScale.x *= -1;
+                    transform.localScale = theScale;
+                }
+            }
+            else
+                canLeft = true;
 
-		if (canUp)
-			targets.Add (upTarget);
-		if (canDown)
-			targets.Add (downTarget);
-		if (canLeft)
-			targets.Add (leftTarget);
-		if (canRight)
-			targets.Add (rightTarget);
-		Debug.Log ((int)targets.Count);
-		rand = Random.Range (0, (int)targets.Count);
+        }
+        else
+            canLeft = true;
 
-		if (targets.Count > 0)
-			targetPos = targets [rand];
-		targets.Clear ();
+        RaycastHit2D rightHit = Physics2D.Raycast(rightStart, Vector2.right, 9f);
+        if (rightHit.collider != null)
+        {
+            if (rightHit.collider.gameObject.tag == "Dirt" || rightHit.collider.gameObject.tag == "Barrier")
+            {
+                canRight = false;
+                Vector3 theScale = transform.localScale;
+
+
+                //turns scale negative on wall impact (to change sprite direction)
+                if (!canRight && theScale.x < 0)
+                {
+                    theScale.x *= -1;
+                    transform.localScale = theScale;
+                }
+            }
+            else
+                canRight = true;
+        }
+        else
+            canRight = true;
+
+        Vector2 upTarget = new Vector2(transform.position.x, transform.position.y + 10f);
+        Vector2 downTarget = new Vector2(transform.position.x, transform.position.y - 10f);
+        Vector2 leftTarget = new Vector2(transform.position.x - 10f, transform.position.y);
+        Vector2 rightTarget = new Vector2(transform.position.x + 10f, transform.position.y);
+
+       
+        if (canUp)
+            targets.Add(upTarget);
+        if (canDown)
+            targets.Add(downTarget);
+        if (canLeft)
+            targets.Add(leftTarget);
+        if (canRight)
+            targets.Add(rightTarget);
+        Debug.Log((int)targets.Count);
+        rand = Random.Range(0, (int)targets.Count);
+
+        if (targets.Count > 0)
+            targetPos = targets[rand];
+        targets.Clear();
+        
 	}
 
 	void Move(){
